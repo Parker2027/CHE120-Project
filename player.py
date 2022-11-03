871b06dd3d4a4a5fd76e3ecea3bdb96032fe8e5c
Parker2027 <117185643+Parker2027@users.noreply.github.com>
2022-11-03 08:43:54 -0400

    Delete player.py

--- player.py
+++
@@ -1,9 +0,0 @@
-import pygame 
-from settings import *
-
-class Player(pygame.sprite.Sprite):
-	def __init__(self,pos,groups):
-		super().__init__(groups)
-		#imports image for the player sprite
-		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
-		self.rect = self.image.get_rect(topleft = pos)
