19d30b0fad42f38e1947a0a405ffcef89554e66f
LXBourassa <116652628+LXBourassa@users.noreply.github.com>
2022-11-29 17:09:48 -0500

    Update player.py

--- code/player.py
+++ code/player.py
@@ -201,24 +201,32 @@ class Player(Entity):
 				self.status = self.status.replace('_attack','') #Replaces attack with an empty string
 
 	def cooldowns(self):
-	#This function acts as a clock for cooldowns, using parts of another module
+	#This function acts as a clock that cooldowns are based on, using parts of another module
 		current_time = pygame.time.get_ticks()
 		
-
+		#Cooldown for attack input
 		if self.attacking:
+			#Compares the attack time to the time taken for an attack cooldown based on specific weapon data
 			if current_time - self.attack_time >= self.attack_cooldown + weapon_data[self.weapon]['cooldown']:
+				#If the time spent in the attacking state is greater than or equal to the cooldown of the attack, the character is no longer attacking
 				self.attacking = False
-				self.destroy_attack()
-
+				self.destroy_attack() #Destroys the sprite of the weapon used to attack
+				
+		#Cooldown for weapon switching, if the input to switch weapons was given
 		if not self.can_switch_weapon:
+			#If the time spent after switching the weapon is greater than or equal to the cooldown for weapon switching, the player is able to switch weapons again
 			if current_time - self.weapon_switch_time >= self.switch_duration_cooldown:
 				self.can_switch_weapon = True
-
+				
+		#Cooldown for switching magic if the input to switch magic is given
 		if not self.can_switch_magic:
+			#If the time taken after switching weapons is greater than or equal to the cooldown for weapon switching, the player is able to switch again
 			if current_time - self.magic_switch_time >= self.switch_duration_cooldown:
+				#Cooldown is the same as for switching weapons
 				self.can_switch_magic = True
-
+		#Cooldown for grace period after taking damage
 		if not self.vulnerable:
+			#If the time taken after being damaged is greater than or equal to the grace period of invulnerability, the character can be damaged again
 			if current_time - self.hurt_time >= self.invulnerability_duration:
 				self.vulnerable = True
 
